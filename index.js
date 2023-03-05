0927d4ff390cde730c3ff67a2b3d56819ff341d3
Leimock <miguelestevezpardo@gmail.com>
2023-03-05 20:31:09 +0100

    little fix

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ import authRouter from "./routers/authRouter.js"
 import connectionRouter from "./routers/connectionRouter.js"
 import passport from "passport"
 import passportMiddleware from "./middlewares/passport.js"
+import { Configuration, OpenAIApi } from "openai"
 
 dotenv.config()
 const app = express()
@@ -22,8 +23,33 @@ app.use(morgan('tiny'))
 app.use(passport.initialize())
 passport.use(passportMiddleware)
 
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  })
+const openai = new OpenAIApi(configuration)
+
+
 app.get('/', (req, res) => {
     res.status(200).send('Bienvenido al API de debeTu')
+})
+
+app.get('/menu', async (req, res) => {
+    const {gender, weight, diseases, period} = req.query
+
+    const prompt = `Genera un menu para ${period} para una pesona con las siguientes caracteristicas: \nGenero: ${gender} \nPeso: ${weight}kg \nEnfermedades: ${diseases}.\n\nMenu:`
+
+    const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt,
+        temperature: 0.5,
+        max_tokens: 64,
+        top_p: 1.0,
+        frequency_penalty: 0.0,
+        presence_penalty: 0.0,
+      })
+
+    res.status(200).json(response.data.choices[0].text)
 })
 
 app.use('/message', messageRouter)
